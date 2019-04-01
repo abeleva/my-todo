c723a4ab0cfcdcc0cd6c3c49f4b4ec7877bb4a82
abeleva <beleva.a@gmail.com>
2019-04-01 23:04:54 +0300

    Fixes after review

--- index.js
+++ index.js
@@ -2,42 +2,30 @@
 import './style.css';
 
 // Объявляем переменные
+const form = document.getElementById('form');
 const field = document.getElementById('field');
-const button = document.getElementById('button');
 const list = document.getElementById('list');
 const listArray = [];
 
-// Тут записываем значение инпута в массив
-function collectItem(arr) {
+// Добавляем события
+form.addEventListener('submit', function(e){
+  e.preventDefault();
 
-  if (!field.value) {
-    return false;
-  } else arr.push(field.value); // Тут надо универсальнее как-то?
-    return arr;
+  const fieldValue = field.value.trim();
 
-}
+  if (!fieldValue) { // Забыла, что тут можно еще добавить
+    return;
+  }
+
+  listArray.push(field.value);
+
+  addItem(listArray);
+});
 
 // А тут новый элемент списка создадим и добавим
 function addItem(arr) {
   let newItem = document.createElement('li');
-  newItem.innerHTML = arr[arr.length - 1];
+  newItem.innerText = arr.slice(-1);
   list.appendChild(newItem);
 }
 
-// Создание элемента списка
-function createListItem(arr) {
-  if (collectItem(arr)) { // Если вернет false не новый элемент не добавится
-    addItem(arr); // Добавляем новый элемент
-  }
-}
-
-// Это нужно объединить? Нужно вообще по-другому?
-button.addEventListener('click', function(){
-  createListItem(listArray);
-});
-
-field.addEventListener('keyup', function (e) {
-    if (e.keyCode === 13) {
-        createListItem(listArray);
-    }
-});
